0d539215e55a68a5d08efc10ebc4706e8200151d
ojncglg <grigori.lopezgarcia@gmail.com>
2024-09-21 19:45:10 -0400

    Update homework.ts

--- src/homework.ts
+++ src/homework.ts
@@ -16,6 +16,7 @@ export const addTwoNumbers = (a: number, b?: number): number => {
 //  */
 export const sumArray = (numbers: (number | string)[]): number => {
   return numbers.reduce((acc: number, curr) => {
+    //if it is a string, it will try to convert it to a number
     const numValue = typeof curr === 'string' ? parseFloat(curr) : curr;
     return acc + (isNaN(numValue) ? 0 : numValue);
   }, 0);
@@ -31,6 +32,7 @@ export type Person = {
 
 // Create type "User" which extends "Person" and adds the following properties:
 // - type: 'user' (literal type)
+//User is all the things that a person plus a type property 'User'
 export type User = Person & {
   type: 'user';
 };
@@ -39,18 +41,21 @@ export type User = Person & {
 // - isSuperAdmin: boolean
 // is this person an admin? 
 export type Admin = Person & {
-  isSuperAdmin: boolean;
+  isAdmin: boolean; 
 };
 
 // Create a type "AllPeople" which is a union of "Person", "User", and "Admin"
+//Allpeople can be a person, a user, or admin 
 export type AllPeople = Person | User | Admin;
 
 // Add function "isAdmin" that returns true if "u" is an admin
+//takes AllPeople object and checks if it has an isAdmin property, if it does u will be treated as an Admin
 export const isAdmin = (u: AllPeople): u is Admin => {
-  return 'isSuperAdmin' in u;
+  return 'isAdmin' in u;  
 };
 
 // Add function "isUser" that returns true if "u" is a user
+//Takes AllPeople, checks if it has a type property AND if it is a user. If both are true then u will be treated as a User
 export const isUser = (u: AllPeople): u is User => {
   return 'type' in u && u.type === 'user';
 };
@@ -63,6 +68,9 @@ export const isUser = (u: AllPeople): u is User => {
  * @returns A greeting message
  */
 export const userGreetingMessage = (u: AllPeople): string => {
+  //checks if the object is an Admin using the isAdmin function, If true, returns a greeting string calling them an admin
+  //checks if object is a User using isUser function.
+  //if not a user or admin, then you dont have access
   if (isAdmin(u)) {
     return `Hello, ${u.name}. You are an admin.`;
   } else if (isUser(u)) {
